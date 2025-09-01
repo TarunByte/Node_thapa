8d576c62e6d894270462aca9a5944edc1c9aeadd
TarunBits <tarunbhattmortal@gmail.com>
2025-09-01 19:57:04 +0530

    Server HTML CSS & JavaScript Files in Express.js | express.static Explained

--- express/app.js
+++ express/app.js
@@ -9,16 +9,17 @@ import path from "path";
 
 const app = express();
 
+// absolute path
+const staticPath = path.join(import.meta.dirname, "public");
+
+app.use(express.static(staticPath));
+
 // console.log(__dirname);
 // console.log(__filename);
 app.get("/", (req, res) => {
   // console.log(import.meta.url);
   // const __filename = new URL(import.meta.url).pathname;
   // console.log(__filename);
-
-  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
-
-  res.sendFile(homePagePath);
 });
 
 app.listen(PORT, () => {
