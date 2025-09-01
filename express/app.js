dfd08e406aee96df65ceebdce8f7f39e191ffac4
TarunBits <tarunbhattmortal@gmail.com>
2025-09-01 22:07:49 +0530

    ES Modules in Node.js - Caveats, __dirname & __filename Fixes | Top-Level Await Explained

--- express/app.js
+++ express/app.js
@@ -9,9 +9,15 @@ import path from "path";
 
 const app = express();
 
+// In newer versions of Node.js (14.8+), you can use top-Level await without needing to wrap it in an async function.
+
+const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+const json = await response.json();
+console.log(json);
+
 // absolute path
 const staticPath = path.join(import.meta.dirname, "public");
-
+console.log(staticPath);
 app.use(express.static(staticPath));
 
 // console.log(__dirname);
