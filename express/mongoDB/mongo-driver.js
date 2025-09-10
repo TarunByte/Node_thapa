9eeca6287f6a5b9c39c21d2d65189ca5af17ad0f
TarunBits <tarunbhattmortal@gmail.com>
2025-09-10 23:03:44 +0530

    MongoDB & Express.js CRUD Operations Tutorial

--- express/mongoDB/mongo-driver.js
+++ express/mongoDB/mongo-driver.js
@@ -6,4 +6,32 @@ await client.connect();
 const db = client.db("mongodb_nodejs_db");
 const userCollection = db.collection("users");
 
-userCollection.insertOne({ name: "vinod technical", age: 22 });
+// userCollection.insertOne({ name: "vinod technical", age: 22 });
+
+// userCollection.insertMany([
+//   { name: "technical", role: "user", age: 22 },
+//   { name: "thapa", role: "user", age: 23 },
+//   { name: "bahadur ", role: "admin", age: 24 },
+// ]);
+
+// Read
+// const userCursor = userCollection.find();
+// console.log(userCursor);
+
+// for await (const user of userCursor) {
+//   console.log(user);
+// }
+
+// const userCursor = await userCollection.find().toArray();
+// console.log(userCursor);
+
+// const user = await userCollection.findOne({ name: "vinod thapa" });
+// console.log(user);
+// console.log(user._id.toHexString());
+
+// Update
+// await userCollection.updateOne({ name: "thapa" }, { $set: { age: 21 } });
+
+// Delete
+const result = await userCollection.deleteMany({ name: "vinod technical" });
+console.log(`${result.deletedCount} documents deleted.`);
