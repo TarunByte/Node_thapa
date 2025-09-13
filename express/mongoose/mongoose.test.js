e8cce32dbd995e5b373a4c30b9d64b496bf45bf8
TarunBits <tarunbhattmortal@gmail.com>
2025-09-13 11:52:15 +0530

    Mongoose + Express CRUD: Perform Create, Read, Update, Delete in MongoDB

--- express/mongoose/mongoose.test.js
+++ express/mongoose/mongoose.test.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 // step 1: to connect to the mongoDB server
 try {
   await mongoose.connect("mongodb://localhost:27017/mongoose_database");
-  mongoose.set("debug", true);
+  // mongoose.set("debug", true);
 } catch (error) {
   console.log(error);
   process.exit();
@@ -20,6 +20,32 @@ const userSchema = mongoose.Schema({
 // step 3: creating a model
 const Users = mongoose.model("user", userSchema);
 
-await Users.create({ name: "thapa", age: 21, email: "thapa@technical.com" });
+// await Users.create({ name: "thapa", age: 21, email: "thapa@technical.com" });
+
+const userData = [
+  { name: "Alice", email: "alice@example.com", age: 20 },
+  { name: "Bob", email: "bob@example.com", age: 19 },
+  { name: "charlie", email: "charlie@example.com", age: 22 },
+];
+
+//? step1: Insert
+// await Users.insertMany(userData);
+
+//? step 2: Read
+// const users = await Users.find();
+// const users = await Users.find({ age: { $gt: 21 } });
+// console.log(users);
+
+//? step 3: Update
+// const updatedUser = await Users.updateOne(
+//   { email: "bob@example.com" },
+//   // { $set: { age: 20 } }
+//   { $inc: { age: 3 } }
+// );
+// console.log(updatedUser);
+
+//? step 4: Delete
+const deletedUser = await Users.deleteOne({ email: "bob@example.com" });
+console.log(deletedUser);
 
 await mongoose.connection.close();
